99dfd7f235d8437a1f0751c06a3ba427bc9f758b
MoonTory <gustavoqnt40@gmail.com>
2019-09-04 02:58:12 -0300

    fix: Footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,47 @@
 import React, { FunctionComponent } from 'react'
 
-import { useGatsbyLogo } from 'hooks'
+import { useGatsbyLogo, useGithubLogo, useNetlifyLogo } from 'hooks'
 
 interface Props {}
 
 export const Footer: FunctionComponent<Props> = () => {
-  const logo = useGatsbyLogo()
+  const gastbyLogo = useGatsbyLogo()
+  const githubLogo = useGithubLogo()
+  const netlifyLogo = useNetlifyLogo()
   const defaultMargin = { margin: '0 1rem 0 1rem' }
 
   return (
-    <div className="container has-background-link is-fluid is-marginless has-text-white">
+    <footer className="container has-background-dark is-fluid is-marginless has-text-white">
       <div className="columns has-text-centered">
-        <div className="column">Section 1</div>
-        <div className="column">Section 2</div>
-        <div className="column">Section 3</div>
-        <div className="column">Section 4</div>
-      </div>
-      <div className="columns">
-        <div className="column has-text-centered" style={defaultMargin}>
-          &copy; Gustavo Quinta • {new Date().getFullYear()} • Built with{' '}
-          <img height="16px" width="16px" src={logo.publicURL} alt="GatsbyJs" />
+        <div className="column">
+          <h1 className="is-size-4 has-text-primary">Menu</h1>
+        </div>
+        <div className="column">
+          <h1 className="is-size-4 has-text-primary">Contact</h1>
+        </div>
+        <div className="column">
+          <h1 className="is-size-4 has-text-primary">Social Media</h1>
         </div>
       </div>
-    </div>
+      <div className="columns has-background-primary">
+        <div
+          className="column has-text-centered is-paddingless is-size-5"
+          style={defaultMargin}
+        >
+          &copy; gustavoquinta • {new Date().getFullYear()} • Built with{' '}
+          <a href="https://github.com/MoonTory">
+            <img height="16px" width="16px" src={gastbyLogo.publicURL} alt="GatsbyJs" />
+          </a>{' '}
+          • Deployed on{' '}
+          <a href="https://netlify.com">
+            <img height="16px" width="16px" src={netlifyLogo.publicURL} alt="GatsbyJs" />
+          </a>{' '}
+          • Open sourced on{' '}
+          <a href="https://github.com/MoonTory">
+            <img height="16px" width="16px" src={githubLogo.publicURL} alt="GatsbyJs" />
+          </a>{' '}
+        </div>
+      </div>
+    </footer>
   )
 }
